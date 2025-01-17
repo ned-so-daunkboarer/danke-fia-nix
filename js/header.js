47dfdf8fa5bdcc4e686fd7cce63e136502c43732
Yes <spammail00112233@gmail.com>
2025-01-17 10:29:10 +0100

    Bissl Clarity eini bringa

--- js/header.js
+++ js/header.js
@@ -2,9 +2,8 @@ let appHeader = `
     <nav class="nav-header">
     <a href="./index.html"><div class="logo">Danke fia nix!</div></a>
     <div class="nav-links">
-        <a href="./about.html">About</a>
-        <a href="#contact">Contact</a>
-        <a href="#legal">Legal</a>
+        <a href="./about.html">Über Mich</a>
+        <a href="./contact.html">Kontakt</a>
     </div>
     </nav>
 `;
